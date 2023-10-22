7db1f2b98c9bcfd5f04db992f5a811a072766424
muflihanto <muflihanto@gmail.com>
2023-10-22 17:19:24 +0700

    transform data into dots(scatterplot)

--- src/graph.ts
+++ src/graph.ts
@@ -8,12 +8,20 @@ const data = _data.map((d) => ({
 
 export function setupGraph(container: HTMLDivElement) {
   // Declare the chart dimensions and margins.
-  const width = 720;
-  const height = 480;
+  const width = 800;
+  const height = 520;
   const marginTop = 20;
   const marginRight = 20;
   const marginBottom = 30;
-  const marginLeft = 40;
+  const marginLeft = 60;
+
+  // Declare styles
+  const styles = {
+    xLabel: "-rotate-90 text-[11px]",
+    dot: "dot stroke-slate-950 opacity-75",
+    legend: "text-[10px]",
+    legendRect: "w-[18px] h-[18px]",
+  };
 
   // Declare the x (horizontal position) scale.
   const x = d3
@@ -30,6 +38,9 @@ export function setupGraph(container: HTMLDivElement) {
     .domain(d3.extent(data, (d) => d.Time) as [Date, Date])
     .range([marginTop, height - marginBottom]);
 
+  // Declare color scale
+  const color = d3.scaleOrdinal(d3.schemeDark2);
+
   // Create the SVG container.
   const svg = d3.create("svg").attr("width", width).attr("height", height);
 
@@ -37,13 +48,68 @@ export function setupGraph(container: HTMLDivElement) {
   svg
     .append("g")
     .attr("transform", `translate(0,${height - marginBottom})`)
+    .attr("id", "x-axis")
     .call(d3.axisBottom(x).tickFormat(d3.format("d")));
 
   // Add the y-axis.
   svg
     .append("g")
     .attr("transform", `translate(${marginLeft},0)`)
+    .attr("id", "y-axis")
     .call(d3.axisLeft(y).tickFormat((d) => d3.timeFormat("%M:%S")(d as Date)));
+
+  // Add the y-axis label.
+  svg
+    .append("text")
+    .attr("class", styles.xLabel)
+    .text("Time in Minutes")
+    .attr("x", "-150")
+    .attr("y", "14");
+
+  // Add dots
+  svg
+    .selectAll(".dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .style("fill", (d) => color(String(d.Doping !== "")))
+    .attr("r", 5)
+    .attr("class", styles.dot)
+    .attr("cx", (d) => x(d.Year))
+    .attr("cy", (d) => y(d.Time))
+    .attr("data-xvalue", (d) => d.Year)
+    .attr("data-yvalue", (d) => d.Time.toString());
+
+  // Add legend group
+  const legendContainer = svg
+    .append("g")
+    .attr("id", "legend")
+    .attr("class", styles.legend);
+
+  // Add legend labels group
+  const legend = legendContainer
+    .selectAll("#legend")
+    .data(color.domain())
+    .enter()
+    .append("g")
+    .attr("class", "legend-label")
+    .attr("transform", (_, i) => `translate(0,${height / 2 - i * 20})`);
+
+  // Add a rect for each legend label
+  legend
+    .append("rect")
+    .attr("x", width - marginRight - 18)
+    .attr("class", styles.legendRect)
+    .style("fill", color);
+
+  // Add a text for each legend label
+  legend
+    .append("text")
+    .attr("x", width - marginRight - 24)
+    .attr("y", 9)
+    .attr("dy", ".35em")
+    .style("text-anchor", "end")
+    .text((d) => `${d === "true" ? "Riders with" : "No"} doping allegations`);
 
   // Append SVG element to the container.
   container.append(svg.node()!);
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { setupGraph } from "./graph";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div class="container mx-auto p-8 flex flex-col items-center">
-    <h1 class="font-bold text-2xl">Doping in Professional Bicycle Racing</h1>
+    <h1 class="font-bold text-2xl" id="title">Doping in Professional Bicycle Racing</h1>
     <div id="container" class="mt-2"></div> 
   </div>
 `;
