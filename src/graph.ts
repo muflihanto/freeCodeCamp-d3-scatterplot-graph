9e85f178a6300c1c188db317e79bc66157a0bd3d
muflihanto <muflihanto@gmail.com>
2023-10-22 19:53:51 +0700

    finished all user stories and passed all test

--- src/graph.ts
+++ src/graph.ts
@@ -8,20 +8,33 @@ const data = _data.map((d) => ({
 
 export function setupGraph(container: HTMLDivElement) {
   // Declare the chart dimensions and margins.
-  const width = 800;
-  const height = 520;
-  const marginTop = 20;
+  const width = 880;
+  const height = 550;
+  const marginTop = 80;
   const marginRight = 20;
   const marginBottom = 30;
   const marginLeft = 60;
 
   // Declare styles
   const styles = {
-    xLabel: "-rotate-90 text-[11px]",
-    dot: "dot stroke-slate-950 opacity-75",
+    tooltip:
+      "w-fit max-w-xs text-sm font-semibold h-auto rounded bg-indigo-500/90 border border-indigo-900 absolute pointer-events-none p-2 flex flex-col [&>span:nth-child(3)]:mt-4",
+    xLabel: "-rotate-90 text-[12px] font-bold",
+    dot: "dot stroke-slate-950 opacity-75 fill-[--fill]",
     legend: "text-[10px]",
-    legendRect: "w-[18px] h-[18px]",
+    legendRect: "w-[18px] h-[18px] fill-[--fill]",
+    title: "font-bold text-3xl",
+    subtitle: "font-semibold text-xl",
   };
+
+  // Add Tooltip
+  const tooltip = d3
+    .select("#container")
+    .append("div")
+    .attr("class", styles.tooltip)
+    .attr("id", "tooltip")
+    .style("opacity", 0)
+    .style("visibility", "hidden");
 
   // Declare the x (horizontal position) scale.
   const x = d3
@@ -39,7 +52,7 @@ export function setupGraph(container: HTMLDivElement) {
     .range([marginTop, height - marginBottom]);
 
   // Declare color scale
-  const color = d3.scaleOrdinal(d3.schemeDark2);
+  const color = d3.scaleOrdinal(d3.schemeSet1);
 
   // Create the SVG container.
   const svg = d3.create("svg").attr("width", width).attr("height", height);
@@ -63,7 +76,7 @@ export function setupGraph(container: HTMLDivElement) {
     .append("text")
     .attr("class", styles.xLabel)
     .text("Time in Minutes")
-    .attr("x", "-150")
+    .attr("x", "-320")
     .attr("y", "14");
 
   // Add dots
@@ -72,22 +85,44 @@ export function setupGraph(container: HTMLDivElement) {
     .data(data)
     .enter()
     .append("circle")
-    .style("fill", (d) => color(String(d.Doping !== "")))
-    .attr("r", 5)
+    .style("--fill", (d) => color(String(d.Doping !== "")))
+    .attr("r", 6)
     .attr("class", styles.dot)
     .attr("cx", (d) => x(d.Year))
     .attr("cy", (d) => y(d.Time))
     .attr("data-xvalue", (d) => d.Year)
-    .attr("data-yvalue", (d) => d.Time.toString());
+    .attr("data-yvalue", (d) => d.Time.toString())
+    .on("mouseover", (event, d) => {
+      tooltip.text("").append("span").text(`${d.Name}: ${d.Nationality}`);
+      tooltip
+        .append("span")
+        .text(`Year: ${d.Year}, Time: ${d3.timeFormat("%M:%S")(d.Time)}`);
+      d.Doping !== "" && tooltip.append("span").text(d.Doping);
+      tooltip
+        .attr("data-year", d.Year)
+        .style("left", `${event.pageX}px`)
+        .style("top", `${event.pageY - 24}px`)
+        .transition()
+        .duration(200)
+        .style("opacity", 1)
+        .style("visibility", "visible");
+    })
+    .on("mouseout", () => {
+      tooltip
+        .transition()
+        .duration(200)
+        .style("opacity", 0)
+        .style("visibility", "hidden");
+    });
 
   // Add legend group
-  const legendContainer = svg
+  const legend = svg
     .append("g")
     .attr("id", "legend")
     .attr("class", styles.legend);
 
   // Add legend labels group
-  const legend = legendContainer
+  const legendLabels = legend
     .selectAll("#legend")
     .data(color.domain())
     .enter()
@@ -96,20 +131,42 @@ export function setupGraph(container: HTMLDivElement) {
     .attr("transform", (_, i) => `translate(0,${height / 2 - i * 20})`);
 
   // Add a rect for each legend label
-  legend
+  legendLabels
     .append("rect")
     .attr("x", width - marginRight - 18)
     .attr("class", styles.legendRect)
-    .style("fill", color);
+    .style("--fill", color);
 
   // Add a text for each legend label
-  legend
+  legendLabels
     .append("text")
     .attr("x", width - marginRight - 24)
     .attr("y", 9)
     .attr("dy", ".35em")
     .style("text-anchor", "end")
     .text((d) => `${d === "true" ? "Riders with" : "No"} doping allegations`);
+
+  // Add Title
+  svg
+    .append("text")
+    .text("Doping in Professional Bicycle Racing")
+    .attr("id", "title")
+    .attr("class", styles.title)
+    .attr("text-anchor", "middle")
+    .attr("id", "title")
+    .attr("x", width / 2)
+    .attr("y", 24);
+
+  // Add Subtitle
+  svg
+    .append("text")
+    .text("35 Fastest times up Alpe d'Huez")
+    .attr("id", "subtitle")
+    .attr("class", styles.subtitle)
+    .attr("text-anchor", "middle")
+    .attr("id", "title")
+    .attr("x", width / 2)
+    .attr("y", 50);
 
   // Append SVG element to the container.
   container.append(svg.node()!);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,7 @@
 import "./style.css";
 import { setupGraph } from "./graph";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div class="container mx-auto p-8 flex flex-col items-center">
-    <h1 class="font-bold text-2xl" id="title">Doping in Professional Bicycle Racing</h1>
-    <div id="container" class="mt-2"></div> 
-  </div>
-`;
+document.querySelector<HTMLDivElement>("#app")!.innerHTML =
+  "<div id='container' class='container mx-auto p-8 flex flex-col items-center mt-2'></div>";
 
 setupGraph(document.querySelector<HTMLDivElement>("#container")!);
